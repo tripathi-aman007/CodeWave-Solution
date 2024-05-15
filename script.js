538ffc14b496ec0c936f639ffa7142ea5cd40237
tripathi-aman007 <106093142+tripathi-aman007@users.noreply.github.com>
2024-05-15 22:27:55 +0530

    Add files via upload
    done

---
+++ script.js
@@ -0,0 +1,7 @@
+function showDropdown() {
+  document.getElementById("coursesMenu").classList.add("show");
+}
+
+function hideDropdown() {
+  document.getElementById("coursesMenu").classList.remove("show");
+}
